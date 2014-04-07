e3290241201162820bd418949bdc9fce82aeb0bf
Thorben Krüger <tkr@ecix.net>
2014-04-07 15:25:37 +0200

    we don't need a goroutine here after all

--- core.go
+++ core.go
@@ -4,10 +4,10 @@ Package gocli implements a simple command line interface (cli) library
 It is a higher-level wrapper around the line editor Liner (https://github.com/peterh/liner),
 mainly adding convenience functions for
 
- - registering commands
- - automatic history
- - tab completion
- - running a REPL-ish loop
+ * registering commands
+ * automatic history
+ * tab completion
+ * running a REPL-ish loop
 
 (May or may not be idiomatic Go, the author is just a pedestrian hacker messing around with an interesting new programming language)
 */
@@ -27,10 +27,10 @@ type Option struct {
 }
 
 type CLI struct {
-	Liner     liner.State
-	Options   map[string]Option
-	Default   Option
-	exit_chan chan int
+	Liner   liner.State
+	Options map[string]Option
+	Default Option
+	looping bool
 }
 
 // AddOption registers a command (cmd), appropriate documentation string (help) and callback function with the CLI
@@ -50,25 +50,21 @@ func (cli *CLI) DefaultOption(function func(args []string) string) {
 
 // Loop is a REPL-inspired loop, prompting for input and running the registered callbacks
 func (cli *CLI) Loop(prompt string) {
-	inner := func() {
-		for {
-			cmd, err := cli.Liner.Prompt(prompt)
-			if err != nil {
-				// l.Println(err)
-				cli.Exit(fmt.Sprintf("error: %q", err.Error()))
+	cli.looping = true
+	for cli.looping {
+		cmd, err := cli.Liner.Prompt(prompt)
+		if err != nil {
+			// l.Println(err)
+			cli.Exit(fmt.Sprintf("error: %q", err.Error()))
+		} else {
+			tmp := strings.Split(cmd, " ")
+			if option, ok := cli.Options[tmp[0]]; ok {
+				fmt.Println(option.Function(tmp[1:]))
 			} else {
-				tmp := strings.Split(cmd, " ")
-				if option, ok := cli.Options[tmp[0]]; ok {
-					fmt.Println(option.Function(tmp[1:]))
-				} else {
-					fmt.Println(cli.Default.Function(tmp))
-				}
-
+				fmt.Println(cli.Default.Function(tmp))
 			}
 		}
 	}
-	go inner()
-	<-cli.exit_chan
 }
 
 // Help prints documentation about all registered Options
@@ -78,11 +74,11 @@ func (cli *CLI) Help(args []string) string {
 
 // Exit terminates the loop, returning the specified message
 func (cli *CLI) Exit(message string) string {
+	cli.looping = false
 	cli.Liner.Close()
-	cli.exit_chan <- 1
 	return message
 }
 
 func MkCLI() CLI {
-	return CLI{*liner.NewLiner(), make(map[string]Option), Option{}, make(chan int)}
+	return CLI{*liner.NewLiner(), make(map[string]Option), Option{}, true}
 }
