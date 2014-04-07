381967e5c7526f21c2b0cdd1808a5422f0597e6c
Thorben Krüger <tkr@ecix.net>
2014-04-07 16:45:22 +0200

    enhanced example

--- examples/clidummy.go
+++ examples/clidummy.go
@@ -4,6 +4,7 @@ clidummy demonstrates the basics of using the gocli library
 package main
 
 import (
+	"fmt"
 	"github.com/benthor/gocli"
 	"strings"
 )
@@ -18,9 +19,15 @@ func main() {
 	// register exit Option with stub callback calling cli.Exit with any further provided tokens as argument
 	cli.AddOption("exit", "exits the input loop", func(args []string) string { return cli.Exit(strings.Join(args, " ")) })
 
+	// register exit Option with stub callback calling cli.Exit with any further provided tokens as argument
+	cli.AddOption("kapitänsmützenabzeichen", "just an example of a long cmd name not breaking the help formatting", func(args []string) string { return cli.Exit(strings.Join(args, " ")) })
+
 	cli.DefaultOption(func(args []string) string {
 		return strings.Join(args, " ")
 	})
 
+	// run the main loop
 	cli.Loop("dummyprompt? ")
+
+	fmt.Println("this part of the code is only reached when the cli loop returns")
 }
